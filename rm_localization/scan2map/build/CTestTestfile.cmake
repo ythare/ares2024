84ab93f45802ec6a09d3c1ea0a301fb0dbd5849c
ythare <2422807388@qq.com>
2024-02-02 18:33:11 +0800

    del

--- rm_localization/scan2map/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ythare/ws_livox/src/scan2map
-# Build directory: /home/ythare/ws_livox/src/scan2map/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
